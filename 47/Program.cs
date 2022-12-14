d622e756b9fc898dfa1bc5e33d0dca2e38ffe678
AlenaFelicita <118136714+AlenaFelicita@users.noreply.github.com>
2022-12-15 00:39:23 +0300

    added 47

---
+++ 47/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+
+Random r = new Random();
+Console.CursorVisible = false;
+while (true)
+{
+    Console.SetCursorPosition(
+        left: r.Next(Console.WindowWidth),
+        top: r.Next(Console.WindowHeight)
+        );
+    Console.Write(r.Next(10));
+    Thread.Sleep (1000);
+}
+
